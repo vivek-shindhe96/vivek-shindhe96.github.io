1e70d0f81b7e859920903f7efa317d49a5e3e086
Vivek Rajesh Shindhe <vivek.rajesh@thetechchefs.in>
2018-09-26 14:53:46 +0530

    Added a Main Index page

--- complete-folder/mainIndex.js
+++ complete-folder/mainIndex.js
@@ -1,3 +1,3 @@
-function throwAlert(floor_number){
-    alert("Floor "+floor_number+" detected.");
+document.getElementById("openCameraActivity").onclick=function(){
+    window.location='file:///home/vivek/ar_web/markerless-ar/vivek-shindhe96.github.io/complete-folder/ar-camera-activity.html'
 }
