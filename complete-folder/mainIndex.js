5d2295712123a870bb82e300f71812e362e0d813
Vivek Rajesh Shindhe <vivek.rajesh@thetechchefs.in>
2018-10-04 23:03:58 +0530

    Send data to android

--- complete-folder/mainIndex.js
+++ complete-folder/mainIndex.js
@@ -1,3 +1,7 @@
 document.getElementById("openCameraActivity").onclick=function(){
     window.location='file:///home/vivek/ar_web/markerless-ar/vivek-shindhe96.github.io/complete-folder/ar-camera-activity.html'
 }
+
+function sendDataToAndroid(){
+    
+}
